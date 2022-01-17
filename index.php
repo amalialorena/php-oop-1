e5f3f683f2d4f8385de2fbff40564751828f4b50
Amalia <amalialorenaferariu@gmail.com>
2022-01-17 15:06:05 +0100

    final result

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <!-- 
    /**
      * DEFINIRE CLASSE Film:
-     *      Attributi:
+     *   ok    Attributi:
      *      - titolo
      *      - sottotiolo
      *      - regista
      * 
      *      Metodi:
-     *      - costruttore che accetta titolo
-     *      - getFullTitle: 
+     *   ok   - costruttore che accetta titolo
+     *   ok   - getFullTitle: 
      *          Se sottotitolo presente:
      *            restituisce "titolo: sottotitolo" 
      *          Se sottotiolo assente:
      *            restituisce "titolo" 
-     *      - __toString: 
+     *    ok  - __toString: 
      *          Se regista presente:
      *            restituisce "fullTitle | regista"
      *          Se regista assente:
@@ -21,12 +21,12 @@
      * 
      * UTILIZZO OGGETTI Film:
      * 
-     *      Generare 3 istanze della classe Film:
+     *    ok  Generare 3 istanze della classe Film:
      *      - uno con solo il titolo
      *      - uno con titolo e sottotitolo
      *      - uno con titolo, sottotiolo e regista
      * 
-     *      Stampare tutti gli oggetti tramite toString (implicito)
+     *    ok Stampare tutti gli oggetti tramite toString (implicito)
      * 
      *  RISULTATO ATTESO:
      * 
@@ -35,5 +35,43 @@
      *      Peter Pan: il ritorno all'isola che non c'e' | Robin Budd
      */ -->
      <?php
-        echo "Hello world!"
+        class Film {
+
+          public $title;
+          public $subtitle;
+          public $movieDirector;
+
+          public function __construct($title) {
+            return $this -> title = $title;
+          }
+
+          public function getFullTitle() {
+            if ($this -> subtitle) {
+              return $this -> title . ": " . $this -> subtitle;
+            } else {
+              return $this ->title;
+            }
+          }
+
+          public function __toString() {
+            if($this -> movieDirector) {
+              return $this -> getFullTitle() . " | " .  $this -> movieDirector . "</br>";
+            } else {
+              return $this -> getFullTitle() . " | " .  " ???" . "</br>";
+            }
+          }
+        }
+        
+        $f1 = new Film("Matrix");
+        
+        $f2 = new Film("Fantozzi 2");
+        $f2 -> subtitle = "il ritorno di fantozzi";
+
+        $f3 = new Film("Peter Pan");
+        $f3 -> subtitle = "il ritorno all'isola che non c'e'";
+        $f3 -> movieDirector = "Robin Budd";
+
+        echo $f1;
+        echo $f2;
+        echo $f3;
      ?>
